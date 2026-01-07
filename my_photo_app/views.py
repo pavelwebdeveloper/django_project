ef11fdb7f1b45054054a6ba80735cadb35c12203
Pavel Shvets <pavel.v.shvets@gmail.com>
2026-01-07 00:16:25 -0700

    changed the display of Hello World from html to a message sent through HttpResponse

--- my_photo_app/views.py
+++ my_photo_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-#from django.http import HttpResponse
+from django.http import HttpResponse
 #from django.template import loader
 
 def home(request):
     #return HttpResponse("Hello world from my_photo_app coded in Python!")
     #template = loader.get_template('home.html')
     #return HttpResponse(template.render())
-    return render(request, "home.html", {})
+    return HttpResponse("Hello world from my_photo_app coded in Python!")
+
+
+    #return render(request, "home.html", {})
